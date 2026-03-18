c56ba81bdbc66fe8ee1869d0293b36da11c28068
Evan Thomas <evan@evanjt.com>
2026-03-18 21:25:22 +0100

    Remove pebble/android targets for now

--- pebble/src/c/messaging.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <pebble.h>
-
-void messaging_init(void);
-void messaging_deinit(void);
-void messaging_request_stations(void);
-void messaging_request_departures(const char *station_id);
--- pebble/src/c/ui.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <pebble.h>
-
-void ui_init(Window *window);
-void ui_deinit(void);
-void ui_update(void);
