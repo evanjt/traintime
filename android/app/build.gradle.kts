1e339f269ac89d0245b1986a73b8aaefdb4ca557
Evan Thomas <evan@evanjt.com>
2026-06-17 22:25:55 +0200

    Bump to 0.4.0 with release notes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.evanjt.traintime"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.4.0"
 
         buildConfigField("String", "TRAINTIME_API_KEY", "\"$traintimeApiKey\"")
     }
